a21128883e867892eedee130421a32613b19fb15
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-12-24 17:08:32 +0100

    working on details

--- src/components/TabContent/TabContent.jsx
+++ src/components/TabContent/TabContent.jsx
@@ -1,15 +1,27 @@
 import "./TabContent.css";
 
-function TabContent({ name, title, description, image }) {
+function TabContent({
+  name,
+  title,
+  description,
+  image,
+  distanceFromSun,
+  diameter,
+  gravity,
+}) {
   return (
     <div id="tabContent">
       <div className="leftSide">
         <img src={image} alt="Planet" />
       </div>
+      <span></span>
       <div className="rightSide">
         <h1>{name}</h1>
         <h2>{title}</h2>
         <p>{description}</p>
+        <div>{distanceFromSun}</div>
+        <div>{diameter}</div>
+        <div>{gravity}</div>
       </div>
     </div>
   );
